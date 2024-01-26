f9b85b6050f460a38dd815a16c326fabbece49df
abhishekgwal <abhishekgwal@gmail.com>
2024-01-26 10:57:08 -0500

    Processing batch requests asynchronously with ten threads

--- src/main/java/com/spring/batch/config/SpringBatchConfig.java
+++ src/main/java/com/spring/batch/config/SpringBatchConfig.java
@@ -17,6 +17,8 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
@@ -73,6 +75,7 @@ public class SpringBatchConfig {
                 .reader(reader())
                 .processor(processor())
                 .writer(writer())
+                .taskExecutor(taskExecutor())
                 .build();
     }
 
@@ -80,5 +83,12 @@ public class SpringBatchConfig {
     public Job runJob(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new JobBuilder("importCustomers", jobRepository)
                 .flow(step1(jobRepository, transactionManager)).end().build();
+    }
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+        asyncTaskExecutor.setConcurrencyLimit(10);
+        return asyncTaskExecutor;
     }
 }
